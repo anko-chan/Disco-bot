a146def7d55f00673a099632e4d180ffe6fbd188
anko-chan <61561412+anko-chan@users.noreply.github.com>
2020-05-14 05:19:13 +0900

    shutdown command done

--- testbot.py
+++ testbot.py
@@ -1,6 +1,26 @@
 import discord
+import configparser
+
+import os
+import errno
 
 client = discord.Client()
+
+#____________________________________
+#config読み込み
+#____________________________________
+config = configparser.ConfigParser()
+config_ini_path = 'settings.ini'
+#configファイルがない場合のエラー
+if not os.path.exists(config_ini_path):
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_ini_path)
+
+config.read(config_ini_path, encoding='utf-8')
+
+#権限設定取得
+boss = config['Boss']['BossID']
+
+#______________________________________
 
 @client.event
 async def on_ready():
@@ -11,8 +31,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    #shutdown command for BOSS
+    if message.content.startswith('!shutdown'):
+        if message.author.id == int(boss):
+            await message.channel.send("boss' calling")
+            await client.close()
+        else:
+            await message.channel.send('youre not boss')
+            return
+
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
+
 
     #diceroll
     #if message.content.startswith('!roll'):
