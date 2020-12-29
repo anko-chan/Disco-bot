5137cfa0a7532325e2dad13fbb6c89e2a86d8d91
anko-chan <61561412+anko-chan@users.noreply.github.com>
2020-12-29 16:34:17 +0900

    basic connect method?

--- dbcontrol.py
+++ dbcontrol.py
@@ -7,9 +7,32 @@ conn = sqlite3.connect(recruitdb)
 c = conn.cursor()
 """
 
-def connectMemberdb():
-    members = 'members.db'
-    connM = sqlite3.connect(members)
+class Memberdb:
 
-    c = connM.cursor()
+    def __init__(self, server):
+        self.server = server
+        #self.cursor = 0
+
+
+    def connectdb():
+        members = server + 'members.db'
+        return sqlite3.connect(members)
+
+
+    def disconnectdb(self, connection):
+        #members = server + 'members.db'
+        #connM = sqlite3.connect(members)
+
+        connection.commit()
+        connection.close()
+
+
+    def createtable():
+        conn = connectdb()
+        c = conn.cursor()
+
+        #(id, name, rating)
+        c.execute(
+            'CREATE TABLE memberlist(id INTEGER PRIMARY KEY, name STRING, rating INTEGER)')
+        conn.disconnectdb(conn)
 #
